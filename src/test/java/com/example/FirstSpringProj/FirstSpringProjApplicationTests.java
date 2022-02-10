9798a88db28f3681e9cd0a8ba5f6d54a0b73696a
shreyaskchavan <48999230+shreyaskchavan@users.noreply.github.com>
2022-02-10 11:58:28 -0600

    Initialize Spring Boot Projet

---
+++ src/test/java/com/example/FirstSpringProj/FirstSpringProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FirstSpringProj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
